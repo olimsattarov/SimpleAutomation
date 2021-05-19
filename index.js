3cd2746421d6a6f07b8150ec6e497ce8e4759cb9
olimsattarov <>
2021-05-19 05:29:48 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+ const fs = require('fs');
+
+ calculate = () => {
+   fs.readFile('data.txt', 'utf8', (err, data) => {
+     if (err) {
+      throw new Error(err)
+     }
+
+    const arr = data.split('\r\n');
+    const result = arr
+        .filter(e => e)
+        .map(parseFloat)
+        .reduce((curr, next) => curr + next);
+    console.log('RESULT:', result);
+   });
+ }
